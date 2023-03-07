5b848e39873144bc53d3c286dd0ae76711b00bef
Em McGlone <mdmcglone@college.harvard.edu>
2023-03-07 21:21:44 +0530

    generated mapping and rendered pieces on board

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,46 @@
 fn main() {
-    let blocktop = "------------";  
-    let blockbody = "|            ";    // block widths as square
+    let blocktop = "-----------";  
+    let blockbody = "|           ";    // block widths as square
 
 
-    let all_pieces = gather_pieces();
+    let mut all_pieces = gather_pieces();
 
     let empty = String::from("x");
-       
-    let map: Vec<Vec<&str>> = vec![
-        vec![&all_pieces[0].match_kind(), &all_pieces[1].match_kind(), &all_pieces[2].match_kind(), &all_pieces[3].match_kind(), &all_pieces[4].match_kind(), &all_pieces[5].match_kind(), &all_pieces[6].match_kind(), &all_pieces[7].match_kind()],
-        vec![&all_pieces[8].match_kind(), &all_pieces[9].match_kind(), &all_pieces[10].match_kind(), &all_pieces[11].match_kind(), &all_pieces[12].match_kind(), &all_pieces[13].match_kind(), &all_pieces[14].match_kind(), &all_pieces[15].match_kind()],
+
+    let mut map: Vec<Vec<&str>> = vec![
         vec![&empty, &empty, &empty, &empty, &empty, &empty, &empty, &empty],
         vec![&empty, &empty, &empty, &empty, &empty, &empty, &empty, &empty],
         vec![&empty, &empty, &empty, &empty, &empty, &empty, &empty, &empty],
         vec![&empty, &empty, &empty, &empty, &empty, &empty, &empty, &empty],
-        vec![&all_pieces[16].match_kind(), &all_pieces[17].match_kind(), &all_pieces[18].match_kind(), &all_pieces[19].match_kind(), &all_pieces[20].match_kind(), &all_pieces[21].match_kind(), &all_pieces[22].match_kind(), &all_pieces[23].match_kind()],
-        vec![&all_pieces[24].match_kind(), &all_pieces[25].match_kind(), &all_pieces[26].match_kind(), &all_pieces[27].match_kind(), &all_pieces[28].match_kind(), &all_pieces[29].match_kind(), &all_pieces[30].match_kind(), &all_pieces[31].match_kind()],
+        vec![&empty, &empty, &empty, &empty, &empty, &empty, &empty, &empty],
+        vec![&empty, &empty, &empty, &empty, &empty, &empty, &empty, &empty],
+        vec![&empty, &empty, &empty, &empty, &empty, &empty, &empty, &empty],
+        vec![&empty, &empty, &empty, &empty, &empty, &empty, &empty, &empty],
+
         ];
-           
+
+    //INNER GAME LOOP SOMEWHERE IN HERE
+    
+    // iterate thru all pieces and get their file and rank
+    for piece in all_pieces.iter_mut() {
+
+        // println!("{:?}", piece.get_file());
+        // println!("{:?}", piece.get_rank());
         
-    let find_rook = map[0][0];
+        let ifile = piece.get_rank();
+        let irank = piece.get_file();
+        
+        let file = *ifile as usize;
+        let rank = *irank as usize;
+        
+        map[file-1][rank-1] = piece.get_kind();
 
-    // println!("{}", find_rook);
+    };
+                  
 
-    // iterate thru all pieces
+    render_board(blocktop, blockbody, map);
 
-    render_board(blocktop, blockbody);
-
-    println!("{:?}", map);
+    // println!("{:?}", map);
 
     // let w = King{
     //     rank: 0,
@@ -53,34 +66,35 @@ fn main() {
 }
 
 
-fn render_board(blocktop: &str, blockbody: &str) {
-    for letter in 1..=8 {  // print block number down
+fn render_board(blocktop: &str, blockbody: &str, map: Vec<Vec<&str>>) {
+    for letter in 0..=7 {  // print block number down
 
         for i in 0..=8 {
-            print!("{blocktop}");
+            print!("{blocktop}"); // print top row
         }
         print!("\n");
-        for i in 0..= 4 {  // print block height
-            for number in 0..=8 {  // print block number across
+        for layer in 0..= 4 {  // print block height
+            for number in 0..=7 {  // print block number across
 
-                // if piece exists, print piece
-                // render piece by splitting the piece's signature at \n into a slice which inserts in the center of blockbody
-                // else, print blockbody
+                let p = render_piece(map[7-letter][number]);
 
+                //insert p[layer] into the center of blockbody
+                let mut blockbody_filled = String::from(blockbody);
+                blockbody_filled.insert_str(5, p[layer]);
 
                 // if number == 0, add rank number
                 if number == 0 {
-                    if i == 2 {
-                        print!("{} {blockbody}", 9 - letter, blockbody=blockbody);
+                    if layer == 2 {
+                        print!("{} {blockbody_filled}", 8 - letter); // print first layer with rank number
                     } else {
-                        print!("  {blockbody}");
+                        print!("  {blockbody_filled}"); // print first layer without rank number
                     }
                 } else {
 
-                print!("{blockbody}"); 
+                print!("{blockbody_filled}"); // print other layers
                 }
             }
-            print!("\n");
+            print!("|\n");
         }
 
     }
@@ -89,7 +103,7 @@ fn render_board(blocktop: &str, blockbody: &str) {
     }
     print!("\n  ");
     for file in ["a","b","c","d","e","f","g","h"] {  // print letters across
-        print!("      {file}      ");
+        print!("      {file}     ");
     }
     print!("\n");
 }
@@ -104,7 +118,7 @@ enum Piece {
 }
 
 impl Piece {
-    fn match_kind(&self) -> &str {
+    fn get_kind(&self) -> &str {
         match self {
             Piece::Knight(p) => { 
                 if p.yt == true {
@@ -151,14 +165,38 @@ impl Piece {
         }
     }
 
+    fn get_rank(&self) -> &i8 {
+        match self {
+            Piece::Knight(p) => &p.rank,
+            Piece::King(p) => &p.rank,
+            Piece::Queen(p) => &p.rank,
+            Piece::Bishop(p) => &p.rank,
+            Piece::Rook(p) => &p.rank,
+            Piece::Pawn(p) => &p.rank,
+        }
+    }
+
+    fn get_file(&self) -> &i8 {
+        match self {
+            Piece::Knight(p) => &p.file,
+            Piece::King(p) => &p.file,
+            Piece::Queen(p) => &p.file,
+            Piece::Bishop(p) => &p.file,
+            Piece::Rook(p) => &p.file,
+            Piece::Pawn(p) => &p.file,
+        }
+    }
+
+
+
     fn find_id(&self) -> &String {
         match self {
-            Piece::Knight(k) => &k.id,
-            Piece::King(k) => &k.id,
-            Piece::Queen(k) => &k.id,
-            Piece::Bishop(k) => &k.id,
-            Piece::Rook(k) => &k.id,
-            Piece::Pawn(k) => &k.id,
+            Piece::Knight(p) => &p.id,
+            Piece::King(p) => &p.id,
+            Piece::Queen(p) => &p.id,
+            Piece::Bishop(p) => &p.id,
+            Piece::Rook(p) => &p.id,
+            Piece::Pawn(p) => &p.id,
         }
     }
 
@@ -208,14 +246,6 @@ impl Pawn {
         }
     }
 
-    fn render(&self) -> [&str; 5] {
-        if self.yt == true {
-            return ["   ()", "  /--\\", "  \\--/", "  /--\\", " /----\\"]
-        } else {
-            return ["   ()", "  /  \\", "  \\  /", "  /  \\", " /    \\"]
-        }
-    }
-
 }
 
 struct Rook {
@@ -245,13 +275,6 @@ impl Rook {
         }
     }
 
-    fn render(&self) -> [&str; 5] {
-        if self.yt == true {
-            return ["|| || ||","|------|"," |----| "," |----| ","|------|"] 
-        } else {
-            return ["|| || ||","|      |"," |    | "," |    | ","|      |"]
-        }
-    }
 }
 
 
@@ -287,13 +310,6 @@ impl Knight {
         }
     }
 
-    fn render(&self) -> [&str; 5] {
-        if self.yt == true {  
-            return ["  ______"," /--- o ","/L -----","\\~~_____","     \\--"]
-        } else { 
-            return ["  ______"," /    o ","/L      ","\\~~_____","     \\__"]
-        }
-    }
 }
 
 
@@ -330,13 +346,6 @@ impl Bishop {
         }
     }
 
-    fn render(&self) -> [&str; 5] {
-        if self.yt == true {  
-            return ["    O    ","   /-\\   ","  /---\\  "," /- + -\\ ","~\\-----/~"]
-        } else { 
-            return ["    O    ","   / \\   ","  /   \\  "," /  +  \\ ","~\\     /~"];
-        }
-    }
 }
 
 
@@ -377,13 +386,6 @@ impl Queen {
         }
     }
 
-    fn render(&self) -> [&str; 5] {
-        if self.yt == true {  
-            return ["O O O O O","\\-|-|-|-/"," \\-----/ ","  \\---/  "," ======= "];
-        } else { 
-            return ["O O O O O","\\ | | | /"," \\     / ","  \\   /  "," ======= "];
-        }
-    }
 }
 
 
@@ -419,13 +421,6 @@ impl King {
         }
     }
 
-    fn render(&self) -> [&str; 5] {
-        if self.yt == true {  
-            return ["  __+__  "," /--|--\\ "," \\--|--/ "," /--|--\\ ","========="];
-        } else { 
-            return ["  __+__  "," /  |  \\ "," \\  |  / "," /  |  \\ ","========="];
-        }
-    }
 }
 
 
@@ -440,55 +435,70 @@ fn on_board_and_diff(rank: &i8, file: &i8, nrank: &i8, nfile: &i8) -> bool {
     }
 }
 
+fn render_piece(kind: &str) -> [&str; 5] {
+    match kind {
+        "WP" => return ["   0   ", "  /-\\  ", "  \\-/  ", "  /-\\  ", " /---\\ "],
+        "BP" => return ["   0   ", "  / \\  ", "  \\ /  ", "  / \\  ", " /   \\ "],
+        "WR" => return ["|| ||| ||","|-------|"," |-----| "," |-----| ","|-------|"],
+        "BR" => return ["|| ||| ||","|       |"," |     | "," |     | ","|       |"],
+        "WN" => return ["  ______"," /--- o ","/L -----","\\~~_____","     \\--"],
+        "BN" => return ["  ______"," /    o ","/L      ","\\~~_____","     \\__"],
+        "WB" => return ["    O    ","   /-\\   ","  /---\\  "," /- + -\\ ","~\\-----/~"],
+        "BB" => return ["    O    ","   / \\   ","  /   \\  "," /  +  \\ ","~\\     /~"],
+        "WQ" => return ["O O O O O","\\-|-|-|-/"," \\-----/ ","  \\---/  "," ======= "],
+        "BQ" => return ["O O O O O","\\ | | | /"," \\     / ","  \\   /  "," ======= "],
+        "WK" => return ["  __+__  "," /--|--\\ "," \\--|--/ "," /--|--\\ ","========="],
+        "BK" => return ["  __+__  "," /  |  \\ "," \\  |  / "," /  |  \\ ","========="],
+        _ => return ["","","","",""],
+    }
+}
+
 
 
 // return a list of all pieces in initial positions
 fn gather_pieces() -> Vec<Piece> {
-    let w_apawn = Piece::Pawn(Pawn{rank:2, file:0, yt:true, id:String::from("w_apawn")});
-    let w_bpawn = Piece::Pawn(Pawn{rank:2, file:1, yt:true, id:String::from("w_bpawn")});
-    let w_cpawn = Piece::Pawn(Pawn{rank:2, file:2, yt:true, id:String::from("w_cpawn")});
-    let w_dpawn = Piece::Pawn(Pawn{rank:2, file:3, yt:true, id:String::from("w_dpawn")});
-    let w_epawn = Piece::Pawn(Pawn{rank:2, file:4, yt:true, id:String::from("w_epawn")});
-    let w_fpawn = Piece::Pawn(Pawn{rank:2, file:5, yt:true, id:String::from("w_fpawn")});
-    let w_gpawn = Piece::Pawn(Pawn{rank:2, file:6, yt:true, id:String::from("w_gpawn")});
-    let w_hpawn = Piece::Pawn(Pawn{rank:2, file:7, yt:true, id:String::from("w_hpawn")});
+    let w_apawn = Piece::Pawn(Pawn{rank:2, file:1, yt:true, id:String::from("w_apawn")});
+    let w_bpawn = Piece::Pawn(Pawn{rank:2, file:2, yt:true, id:String::from("w_bpawn")});
+    let w_cpawn = Piece::Pawn(Pawn{rank:2, file:3, yt:true, id:String::from("w_cpawn")});
+    let w_dpawn = Piece::Pawn(Pawn{rank:2, file:4, yt:true, id:String::from("w_dpawn")});
+    let w_epawn = Piece::Pawn(Pawn{rank:2, file:5, yt:true, id:String::from("w_epawn")});
+    let w_fpawn = Piece::Pawn(Pawn{rank:2, file:6, yt:true, id:String::from("w_fpawn")});
+    let w_gpawn = Piece::Pawn(Pawn{rank:2, file:7, yt:true, id:String::from("w_gpawn")});
+    let w_hpawn = Piece::Pawn(Pawn{rank:2, file:8, yt:true, id:String::from("w_hpawn")});
     
-    let w_arook = Piece::Rook(Rook{rank:1, file:0, yt:true, id:String::from("w_arook")});
-    let w_bknight = Piece::Knight(Knight{rank:1, file:1, yt:true, id:String::from("w_bknight")});
-    let w_cbishop = Piece::Bishop(Bishop{rank:1, file:2, yt:true, id:String::from("w_cbishop")});
-    let w_queen = Piece::Queen(Queen{rank:1, file:3, yt:true, id:String::from("w_queen")});
-    let w_king = Piece::King(King{rank:1, file:4, yt:true, id:String::from("w_king")});
-    let w_fbishop = Piece::Bishop(Bishop{rank:1, file:5, yt:true, id:String::from("w_fbishop")});
-    let w_gknight = Piece::Knight(Knight{rank:1, file:6, yt:true, id:String::from("w_gknight")});
-    let w_hrook = Piece::Rook(Rook{rank:1, file:7, yt:true, id:String::from("w_hrook")});
+    let w_arook = Piece::Rook(Rook{rank:1, file:1, yt:true, id:String::from("w_arook")});
+    let w_bknight = Piece::Knight(Knight{rank:1, file:2, yt:true, id:String::from("w_bknight")});
+    let w_cbishop = Piece::Bishop(Bishop{rank:1, file:3, yt:true, id:String::from("w_cbishop")});
+    let w_queen = Piece::Queen(Queen{rank:1, file:4, yt:true, id:String::from("w_queen")});
+    let w_king = Piece::King(King{rank:1, file:5, yt:true, id:String::from("w_king")});
+    let w_fbishop = Piece::Bishop(Bishop{rank:1, file:6, yt:true, id:String::from("w_fbishop")});
+    let w_gknight = Piece::Knight(Knight{rank:1, file:7, yt:true, id:String::from("w_gknight")});
+    let w_hrook = Piece::Rook(Rook{rank:1, file:8, yt:true, id:String::from("w_hrook")});
 
-    let b_apawn = Piece::Pawn(Pawn{rank:7, file:0, yt:false, id:String::from("b_apawn")});
-    let b_bpawn = Piece::Pawn(Pawn{rank:7, file:1, yt:false, id:String::from("b_bpawn")});
-    let b_cpawn = Piece::Pawn(Pawn{rank:7, file:2, yt:false, id:String::from("b_cpawn")});
-    let b_dpawn = Piece::Pawn(Pawn{rank:7, file:3, yt:false, id:String::from("b_dpawn")});
-    let b_epawn = Piece::Pawn(Pawn{rank:7, file:4, yt:false, id:String::from("b_epawn")});
-    let b_fpawn = Piece::Pawn(Pawn{rank:7, file:5, yt:false, id:String::from("b_fpawn")});
-    let b_gpawn = Piece::Pawn(Pawn{rank:7, file:6, yt:false, id:String::from("b_gpawn")});
-    let b_hpawn = Piece::Pawn(Pawn{rank:7, file:7, yt:false, id:String::from("b_hpawn")});
+    let b_apawn = Piece::Pawn(Pawn{rank:7, file:1, yt:false, id:String::from("b_apawn")});
+    let b_bpawn = Piece::Pawn(Pawn{rank:7, file:2, yt:false, id:String::from("b_bpawn")});
+    let b_cpawn = Piece::Pawn(Pawn{rank:7, file:3, yt:false, id:String::from("b_cpawn")});
+    let b_dpawn = Piece::Pawn(Pawn{rank:7, file:4, yt:false, id:String::from("b_dpawn")});
+    let b_epawn = Piece::Pawn(Pawn{rank:7, file:5, yt:false, id:String::from("b_epawn")});
+    let b_fpawn = Piece::Pawn(Pawn{rank:7, file:6, yt:false, id:String::from("b_fpawn")});
+    let b_gpawn = Piece::Pawn(Pawn{rank:7, file:7, yt:false, id:String::from("b_gpawn")});
+    let b_hpawn = Piece::Pawn(Pawn{rank:7, file:8, yt:false, id:String::from("b_hpawn")});
 
-    let b_arook = Piece::Rook(Rook{rank:8, file:0, yt:false, id:String::from("b_arook")});
-    let b_bknight = Piece::Knight(Knight{rank:8, file:1, yt:false, id:String::from("b_bknight")});
-    let b_cbishop = Piece::Bishop(Bishop{rank:8, file:2, yt:false, id:String::from("b_cbishop")});
-    let b_queen = Piece::Queen(Queen{rank:8, file:3, yt:false, id:String::from("b_queen")});
-    let b_king = Piece::King(King{rank:8, file:4, yt:false, id:String::from("b_king")});
-    let b_fbishop = Piece::Bishop(Bishop{rank:8, file:5, yt:false, id:String::from("b_fbishop")});
-    let b_gknight = Piece::Knight(Knight{rank:8, file:6, yt:false, id:String::from("b_gknight")});
-    let b_hrook = Piece::Rook(Rook{rank:8, file:7, yt:false, id:String::from("b_hrook")});
+    let b_arook = Piece::Rook(Rook{rank:8, file:1, yt:false, id:String::from("b_arook")});
+    let b_bknight = Piece::Knight(Knight{rank:8, file:2, yt:false, id:String::from("b_bknight")});
+    let b_cbishop = Piece::Bishop(Bishop{rank:8, file:3, yt:false, id:String::from("b_cbishop")});
+    let b_queen = Piece::Queen(Queen{rank:8, file:4, yt:false, id:String::from("b_queen")});
+    let b_king = Piece::King(King{rank:8, file:5, yt:false, id:String::from("b_king")});
+    let b_fbishop = Piece::Bishop(Bishop{rank:8, file:6, yt:false, id:String::from("b_fbishop")});
+    let b_gknight = Piece::Knight(Knight{rank:8, file:7, yt:false, id:String::from("b_gknight")});
+    let b_hrook = Piece::Rook(Rook{rank:8, file:8, yt:false, id:String::from("b_hrook")});
 
 
 
-    let pieces = vec![
-                      b_arook, b_bknight, b_cbishop, b_queen, b_king, b_fbishop, b_gknight, b_hrook,
+    let pieces = vec![w_apawn, w_bpawn, w_cpawn, w_dpawn, w_epawn, w_fpawn, w_gpawn, w_hpawn,
+                      w_arook, w_bknight, w_cbishop, w_queen, w_king, w_fbishop, w_gknight, w_hrook,
                       b_apawn, b_bpawn, b_cpawn, b_dpawn, b_epawn, b_fpawn, b_gpawn, b_hpawn, 
-                      w_apawn, w_bpawn, w_cpawn, w_dpawn, w_epawn, w_fpawn, w_gpawn, w_hpawn,
-                      w_arook, w_bknight, w_cbishop, w_queen, w_king, w_fbishop, w_gknight, w_hrook, 
-                      ];
-
+                      b_arook, b_bknight, b_cbishop, b_queen, b_king, b_fbishop, b_gknight, b_hrook];
 
     return pieces;
 }
@@ -496,32 +506,32 @@ fn gather_pieces() -> Vec<Piece> {
 
 
 // white pawn
-//   ()
-//  /--\
-//  \--/
-//  /--\
-// /----\
+//   0
+//  /-\
+//  \-/
+//  /-\
+// /---\
 
 // black pawn
-//   ()
-//  /  \
-//  \  /
-//  /  \
-// /    \
+//   0
+//  / \
+//  \ /
+//  / \
+// /   \
 
 // white rook
-// || || ||
-// |------|
-//  |----|
-//  |----|
-// |------|
+// || ||| ||
+// |-------|
+//  |-----|
+//  |-----|
+// |-------|
 
 // black rook
-// || || ||
-// |      |
-//  |    |
-//  |    |
-// |      |
+// || ||| ||
+// |       |
+//  |     |
+//  |     |
+// |       |
 
 // white knight
 //    ______
@@ -580,3 +590,12 @@ fn gather_pieces() -> Vec<Piece> {
 //  \  |  /
 //  /  |  \
 // =========
+
+
+// color square white:
+// -------------
+// |···········|
+// |···········|
+// |···········|
+// |···········|
+// -------------
